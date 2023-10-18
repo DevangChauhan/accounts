5ca02cb8a91013a95e01e857890437ca6d3041ac
Devang Chauhan <chauhan.devam@gmail.com>
2023-10-18 15:50:38 +0530

    accounts-23: implement a REST endpoint to get build version details by configuring build version property using @Value annotation

--- src/main/java/com/easybank/accounts/AccountsApplication.java
+++ src/main/java/com/easybank/accounts/AccountsApplication.java
@@ -7,7 +7,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
