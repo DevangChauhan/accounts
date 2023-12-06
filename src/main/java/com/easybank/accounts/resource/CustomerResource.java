0adb46f7c8e083ecc931cc5ac917d7fe934ad79f
Devang Chauhan <chauhan.devam@gmail.com>
2023-12-06 20:35:37 +0530

    ebz-22: implemented the cross-cutting concerns like logging and tracing in gateway-server

--- src/main/java/com/easybank/accounts/resource/CustomerResource.java
+++ src/main/java/com/easybank/accounts/resource/CustomerResource.java
@@ -10,14 +10,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "REST APIs for Customers in EasyBank",
@@ -26,8 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
+@Slf4j
 public class CustomerResource {
-
     private final CustomersService customersService;
 
     public CustomerResource(CustomersService customersService) {
@@ -53,11 +51,16 @@ public class CustomerResource {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestParam
-                                                                   @Pattern(regexp = "(^$|[0-9{10}])", message = "Mobile number must be 10 digits")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id")
+                                                                   String correlationId,
+                                                                   @RequestParam @Pattern(regexp="(^$|[0-9]{10})",
+                                                                           message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        CustomerDetailsDto customerDetailsDto = customersService.fetchCustomerDetails(mobileNumber);
+        log.debug("eazyBank-correlation-id found: {} ", correlationId);
+        CustomerDetailsDto customerDetailsDto = customersService.fetchCustomerDetails(mobileNumber, correlationId);
         return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
+
     }
+
 
 }
