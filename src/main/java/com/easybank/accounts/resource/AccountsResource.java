d8195833c428fa47835d90efa21366d84ce3e2af
Devang Chauhan <chauhan.devam@gmail.com>
2023-12-08 18:21:46 +0530

    ebz-30: implemented a ratelimiter mechanism for accounts & gatewayserver services

--- src/main/java/com/easybank/accounts/resource/AccountsResource.java
+++ src/main/java/com/easybank/accounts/resource/AccountsResource.java
@@ -6,6 +6,7 @@ import com.easybank.accounts.dto.CustomerDto;
 import com.easybank.accounts.dto.ErrorResponseDto;
 import com.easybank.accounts.dto.ResponseDto;
 import com.easybank.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -227,10 +228,17 @@ public class AccountsResource {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("Java17"));
     }
 
     @Operation(
